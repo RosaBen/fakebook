5e752df0abbf6de374e940e704ea74bfc77555d3
RosaBen <benchabane.rosa@outlook.com>
2025-06-12 18:56:56 +0200

    added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,16 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require "faker"
+
+puts "🔄 Nettoyage des données..."
+User.delete_all
+ActiveRecord::Base.connection.execute("DELETE FROM sqlite_sequence WHERE name='users'")
+
+puts "👤 Ajout des utilisateurs"
+User.create!(email: "test@test.com", password_digest: "password")
+10.times do
+  User.create!(
+    email: Faker::Internet.unique.email,
+    password_digest: "password"
+  )
+end
+
+puts "✅ Seeds terminés !"
